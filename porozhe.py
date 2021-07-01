798c0f4faa9780420512fc789a7fec0989ad57b8
mehrana1380 <84845533+mehrana1380@users.noreply.github.com>
2021-07-01 04:33:33 -0700

    Add files via upload

--- porozhe.py
+++ porozhe.py
@@ -1,7 +1,21 @@
 class Cat:
-    name= "tedi"
-    age= 2
+    name=""
+    age= 0
+    color=""
 
     def hello():
         print("miav miav")
-Cat.hello()
+
+tadi=Cat()
+tadi.name="alfred"
+tadi.age=7
+tadi.color="black"
+
+rex=Cat()
+rex.name="rex"
+rex.age=2
+rex.color="with"
+
+print(rex.name)
+
+tadi.hello()
